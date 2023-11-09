b249cdaf8879242475b8f7de61c6e1fa6a8f27dd
Rossouw93 <139475020+Rossouw93@users.noreply.github.com>
2023-11-09 19:30:17 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,61 @@
+// my age is 30
+let myAge = 30;
+
+// early years is 2 and will change
+let earlyYears = 2
+
+// early years is 10.5 years per year.
+earlyYears *= 10.5;
+
+// laterYears is equal to myAge - 2.
+let laterYears = myAge - 2; 
+
+// later years is equal to 4 times a normal year.
+laterYears *= 4;
+
+// code test thus far.
+console.log(earlyYears);
+console.log(laterYears);
+
+// dog years is equal to lateryears plus early years.
+const myAgeInDogYears = laterYears + earlyYears;
+
+// myNameNormal is set to Francois, and the string method is used to set it to all lowercase in myName. 
+let myNameNormal = 'Francois';
+let myName = myNameNormal.toLowerCase();
+
+// myName, myAge, and myAgeInDogYears are inserted through string interpolation.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+// Extra Practice Project
+
+
+// my age is 30
+// let myAge = 28;
+
+// early years is 2 and will change
+// let earlyYears = 2
+
+// early years is 10.5 years per year.
+// earlyYears = earlyYears * 10.5;
+
+// laterYears is equal to myAge - 2.
+// let laterYears = myAge - 2; 
+
+// later years is equal to 4 times a normal year.
+// laterYears = laterYears * 4;
+
+// code test thus far.
+// console.log(earlyYears);
+// console.log(laterYears);
+
+// dog years is equal to lateryears plus early years.
+// const myAgeInDogYears = laterYears + earlyYears;
+
+// myNameNormal is set to Francois, and the string method is used to set it to all lowercase in myName. 
+// let myNameNormal = 'Francois';
+// let myName = myNameNormal.toLowerCase();
+
+// myName, myAge, and myAgeInDogYears are inserted through string interpolation.
+// console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
